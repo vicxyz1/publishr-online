a432c3ba75e8508b36d5b24724deecd913ca7a74
vicxyz1 <vicxyz@gmail.com>
2017-09-10 09:53:34 +0300

    updated schema

--- sql/flicker.sql
+++ sql/flicker.sql
@@ -1,18 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.4.13.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Feb 25, 2016 at 05:37 PM
--- Server version: 5.6.25-0ubuntu1
--- PHP Version: 5.6.11-1ubuntu3.1
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
---
--- Database: `flicker`
---
+
 
 -- --------------------------------------------------------
 
@@ -20,16 +11,17 @@ SET time_zone = "+00:00";
 -- Table structure for table `photos`
 --
 
-DROP TABLE IF EXISTS `photos`;
-CREATE TABLE `photos` (
+CREATE TABLE IF NOT EXISTS `photos` (
   `photo_id` int(11) NOT NULL,
-  `publish_time` datetime DEFAULT NULL,
+  `publish_time` int(11) DEFAULT NULL,
   `flickr_photo_id` varchar(45) DEFAULT NULL,
   `flickr_tags` varchar(250) DEFAULT NULL,
-  `fickr_groups` text,
+  `flickr_groups` text,
   `status` tinyint(4) DEFAULT '-1',
   `auth_token` varchar(45) DEFAULT NULL,
-  `url` varchar(250) DEFAULT NULL
+  `auth_secret` varchar(200) NOT NULL DEFAULT '',
+  `url` varchar(250) DEFAULT NULL,
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -38,8 +30,7 @@ CREATE TABLE `photos` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL,
   `flickr_user_id` int(11) NOT NULL,
   `ctime` int(11) NOT NULL,
