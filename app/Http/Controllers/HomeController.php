e0f646ff275c5083d576dbd57e300d40688bef1c
vicxyz1 <vicxyz@gmail.com>
2017-12-04 22:34:24 +0200

    send mail

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
 {
-    public function contact(Request $request) {
+    public function contact(Request $request)
+    {
         $site_name = env('APP_NAME');
 
         return view('contact', compact('site_name'));
     }
 
-    public function contactSend(Request $request) {
+    public function contactSend(Request $request)
+    {
 
         $this->validate($request, [
             'name' => 'required',
@@ -20,7 +23,19 @@ class HomeController extends Controller
             'message' => 'required'
         ]);
 
-        return back()->with('success', 'Thanks for contacting us!');
+
+        Mail::send('email.contact',
+            array(
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'user_message' => $request->get('message')
+            ), function ($message) use ($request) {
+                $to = env('MAIL_ADMIN');
+                $message->to($to, 'Admin')->subject('Publishr Feedback');
+                $message->replyTo($request->get('email'),$request->get('name'));
+            });
+
+         return back()->with('success', 'Thanks for contacting us!');
 
 
     }
